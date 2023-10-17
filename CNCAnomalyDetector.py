c7b9c88b6a56c3883a557a5430adefb63232266b
Julian Rendell <juliangrendell@gmail.com>
2023-10-16 21:15:38 -0700

    fixing git ignore & initial jupyter notebook work

--- CNCAnomalyDetector.py
+++ CNCAnomalyDetector.py
@@ -14,7 +14,55 @@
 # ---
 
 # %% [markdown]
-# # This is a description
+# # Experiment: FFTs on Input Data
 
 # %%
 print("hello world")
+
+# %%
+from pydub import AudioSegment
+
+# %%
+import numpy as np
+
+# %%
+import plotly.graph_objects as go
+
+# %%
+cncaudio = AudioSegment.from_mp3("data/experiments/cncsounds.mp3")
+
+# %%
+if cncaudio.channels >1 :
+    cncaudio = cncaudio.set_channels(1)
+
+
+# %%
+samples = np.array(cncaudio.get_array_of_samples())
+
+# %%
+# Compute the FFT
+
+# %%
+frequencies = np.fft.rfftfreq(len(samples), 1/cncaudio.frame_rate)
+
+# %%
+spectrum = np.abs(np.fft.rfft(samples))
+
+# %%
+fig = go.Figure()
+
+# %%
+# Add the data to the plot
+fig.add_trace(go.Scatter(x=frequencies, y=spectrum, mode='lines'))
+# Set the axes type to loglog
+fig.update_layout(
+    xaxis_type="log",
+    yaxis_type="log",
+    title='Fourier Spectrum of Audio',
+    xaxis_title='Frequency (Hz)',
+    yaxis_title='Amplitude'
+)
+# Display the plot
+fig.show()
+
+# %%
